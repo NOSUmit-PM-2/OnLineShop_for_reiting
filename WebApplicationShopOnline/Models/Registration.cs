fa317d23ad34b68eae23a80aa21367f8f1086b9c
DzhagaevSarmat <dzhagaevsarmat@gmail.com>
2025-06-10 10:32:22 +0300

    Джагаев С.В. (21 гр.) - Часть 2. Задача № 3. Валидация регистрации пользователя.

--- WebApplicationShopOnline/Models/Registration.cs
+++ WebApplicationShopOnline/Models/Registration.cs
@@ -1,9 +1,18 @@
-﻿namespace WebApplicationShopOnline.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplicationShopOnline.Models
 {
     public class Registration
     {
+        [Required(ErrorMessage = "Введите имя пользователя")]
         public string UserName { get; set; }
+
+        [Required(ErrorMessage = "Введите email")]
+        [EmailAddress(ErrorMessage = "Некорректный email")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Введите пароль")]
+        [MinLength(6, ErrorMessage = "Пароль должен быть не менее 6 символов")]
         public string Password { get; set; }
     }
 }
