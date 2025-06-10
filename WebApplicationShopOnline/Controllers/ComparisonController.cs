73912be6ff3a445855e0c0454403eaf7c48315a1
sarmatt3 <alantsarakov@vk.com>
2025-06-10 10:27:39 +0300

    Абазов Аслан 21(2)группа задача 1.10

---
+++ WebApplicationShopOnline/Controllers/ComparisonController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplicationShopOnline.Models;
+
+namespace WebApplicationShopOnline.Controllers
+{
+    public class ComparisonController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult CompareProducts(Product product)
+        {
+            return View(product);
+        }
+    }
+}
+
